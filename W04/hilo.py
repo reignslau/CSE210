e19d9b3fc8d59cfd5241331bdb8d6b4629dce8d3
reignslau <105128253+reignslau@users.noreply.github.com>
2022-10-07 02:27:42 -0600

    Update hilo.py

--- W04/hilo.py
+++ W04/hilo.py
@@ -1,32 +1,30 @@
-import random
+from random import randint
+score = int(10)
 
-value1 = 0
-value2 = 0
-score = 300
-
-
-value1 = random.randint(1,13)
-value2 = random.randint(1,13)
-while True:
-         print("The card is: ",value1)
-
-         x = input("Higher or lower? [h/l]")
-
-         print("Next card was: ",value2)
-         if x == 'h':
-                 if value2 > value1:
-                         print("Your score is: "+str(score+100))
-                 if value2 < value1:
-                         print("Your socre is: "+str(score-75))
-         if x =="l":
-                 if value2 < value1:
-                         print("Your score is: "+str(score+100))
-                 if value2 > value1:
-                         print("Your socre is: "+str(score-75))
-         play_again = input("Play again? [y/n]")
-         if play_again == "y":
-                  continue
-         else:
-                 break
-         
-                        
+play = input("Let's start the game you have 300 points. [y/n]")
+while play =="y":
+     number1 = randint(1,13)
+     number2 = randint(1,13)
+     print("The card is: ",number1)
+     guess = input("Higher or lower? [h/l]")
+     print("Next card was: ",number2)
+     if guess =="h":
+         if number2 > number1:
+             score = score + 100
+             print("Your score is: ", score)
+         if number2 < number1:
+             score = score - 75
+             print("Your score is: ", score)
+     if guess =="l":
+         if number2 < number1:
+             score = score + 100
+             print("Your score is: ", score)
+         if number2 > number1:
+             score = score - 75
+             print("Your score is: ", score)
+     if score > 0:
+         play = input("Play again? [y/n]")
+     if score < 0:
+         play = input("You loss! Do want want to play again? [y/n]")
+if play == "n":
+    print("thanks for play!")   
